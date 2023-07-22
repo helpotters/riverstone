9848eb385ee90bb4801298c54933f7b22e6a1970
Paul Lemus <paullemus@protonmail.com>
2023-07-21 19:22:17 -0700

    feat: product collection display

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -8,35 +8,48 @@ export function HomePage({ data }) {
   const { edges: posts } = data.allMarkdownRemark;
   return (
     <Layout>
-      {posts
-        .filter((post) => post.node.frontmatter.title.length > 0)
-        .map(({ node: post }) => {
-          return (
-            <article
-              key={post.id}
-              className="hover:animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s]"
-            >
-              <div className="rounded-[10px] bg-white p-4 !pt-20 sm:p-6">
-                <time
-                  datetime={post.frontmatter.date}
-                  className="block text-xs text-gray-500"
-                >
-                  {post.frontmatter.date}
-                </time>
-                <a href={`${post.fields.slug}`} className="">
-                  <h3 className="mt-0.5 text-lg no-underline font-medium text-gray-900">
-                    {post.frontmatter.title}
-                  </h3>
-                </a>
-                <div className="mt-4 flex flex-wrap gap-1">
-                  <span className="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600">
-                    Snippet
-                  </span>
-                </div>
-              </div>
-            </article>
-          );
-        })}
+      <section>
+        <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
+          <header className="text-center">
+            <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
+              Products
+            </h2>
+
+            <p className="max-w-md mx-auto mt-4 text-gray-500">
+              Catalog products.
+            </p>
+          </header>
+          <ul className="grid gap-4 mt-8 sm:grid-cols-2 lg:grid-cols-4">
+            {/* Product Items */}
+            {posts
+              .filter((post) => post.node.frontmatter.title.length > 0)
+              .map(({ node: post }) => {
+                return (
+                  <li>
+                    <Link to={post.fields.slug} className="block overflow-hidden group">
+                      <img
+                        src="a-placeholder_image_url"
+                        alt=""
+                        className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px] bg-blue-400"
+                      />
+
+                      <div className="relative pt-3 bg-white">
+                        <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
+                          {post.frontmatter.title}
+                        </h3>
+
+                        <p className="mt-2">
+                          <span className="sr-only"> Date </span>
+                          <span className="tracking-wider text-gray-900">{post.frontmatter.date}</span>
+                        </p>
+                      </div>
+                    </Link>
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
+      </section>
     </Layout>
   );
 }
